00a248376f650fc0db4bdbbdbee9f1c3598a731b
tgszwdv <102832569+tgszwdv@users.noreply.github.com>
2024-08-19 08:13:34 -0400

    Update processosAbertos.js

--- pages/api/processosAbertos.js
+++ pages/api/processosAbertos.js
@@ -1,5 +1,24 @@
-// pages/api/getMongoData.js
 import mongoose from 'mongoose';
+import Cors from 'cors';
+
+// Configuração CORS
+const cors = Cors({
+  origin: '*', // Permite todas as origens
+  methods: ['GET'], // Permite apenas GET
+  allowedHeaders: ['Content-Type'], // Permite cabeçalhos específicos
+});
+
+// Função para executar middlewares em Next.js
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 const mongoURI = process.env.MONGO_URI;
 
@@ -25,10 +44,11 @@ const processoSchema = new mongoose.Schema({
 const Processo = mongoose.models.Processo || mongoose.model('Processo', processoSchema);
 
 export default async function handler(req, res) {
-  await connectToMongoDB();
+  await runMiddleware(req, res, cors); // Aplica CORS
 
   if (req.method === 'GET') {
     try {
+      await connectToMongoDB();
       const processos = await Processo.find({});
       res.status(200).json({ processos });
     } catch (error) {
